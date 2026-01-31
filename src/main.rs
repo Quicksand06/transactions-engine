107c692891bb9b5c1bb3730826b24af88db67689
quicksand <no@email>
2026-01-31 23:35:01 +0100

    store the events and projections

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use crate::commands::Command;
+use crate::event_store::EventStore;
+use crate::events::Event;
+use crate::projections::{ProjectionStore, TransactionType};
 use csv::StringRecord;
 use std::env;
 use std::error::Error;
 use std::fs::File;
 
 mod commands;
+mod event_store;
 mod events;
+mod projections;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path = env::args()
@@ -17,7 +22,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         .trim(csv::Trim::All)
         .from_reader(file);
 
-    let mut event_store = events::EventStore::default();
+    let mut event_store = EventStore::default();
+    let mut projection_store = ProjectionStore::default();
     for result in reader.records() {
         let record = match result {
             Ok(record) => record,
@@ -26,7 +32,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         if let Some(cmd) = parse_command(&record) {
             println!("{:?}", cmd);
+            handle_command(&mut event_store, &mut projection_store, cmd);
         }
+    }
+    let output = projection_store.output();
+    for o in output {
+        println!("{}", o);
     }
 
     Ok(())
@@ -57,5 +68,59 @@ fn parse_command(record: &StringRecord) -> Option<Command> {
         "resolve" => Some(Command::Resolve { client_id, trx_id }),
         "chargeback" => Some(Command::Chargeback { client_id, trx_id }),
         _ => None, // unknown command, so ignore
+    }
+}
+
+pub fn handle_command(
+    event_store: &mut EventStore,
+    projection_store: &mut ProjectionStore,
+    cmd: Command,
+) {
+    match cmd {
+        Command::Deposit {
+            client_id,
+            trx_id,
+            amount,
+        } => {
+            let event = Event::AmountDeposited { trx_id, amount };
+            projection_store.update_for_client(client_id, &event);
+            let a =
+                projection_store.insert_transaction(client_id, trx_id, TransactionType::Deposit);
+            event_store.apply(client_id, event);
+        }
+        Command::Withdrawal {
+            client_id,
+            trx_id,
+            amount,
+        } => {
+            let client = projection_store.get_client_account(client_id);
+            // check if a client has enough funds
+            if client.available < amount {
+                return;
+            }
+
+            let event = Event::AmountWithdrawn { trx_id, amount };
+            projection_store.update_for_client(client_id, &event);
+            projection_store.insert_transaction(client_id, trx_id, TransactionType::Withdrawal);
+            event_store.apply(client_id, event);
+        }
+        Command::Dispute { client_id, trx_id } => {
+            let event = Event::DisputeRaised { trx_id };
+            projection_store.update_for_client(client_id, &event);
+            projection_store.insert_transaction(client_id, trx_id, TransactionType::Dispute);
+            event_store.apply(client_id, event);
+        }
+        Command::Resolve { client_id, trx_id } => {
+            let event = Event::DisputeResolved { trx_id };
+            projection_store.update_for_client(client_id, &event);
+            projection_store.insert_transaction(client_id, trx_id, TransactionType::Resolve);
+            event_store.apply(client_id, event);
+        }
+        Command::Chargeback { client_id, trx_id } => {
+            let event = Event::ChargebackIssued { trx_id };
+            projection_store.update_for_client(client_id, &event);
+            projection_store.insert_transaction(client_id, trx_id, TransactionType::Chargeback);
+            event_store.apply(client_id, event);
+        }
     }
 }
---
+++ src/projections.rs
@@ -0,0 +1,120 @@
+use crate::events::Event;
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct ClientAccount {
+    pub total: f64,
+    pub held: f64,
+    pub available: f64,
+    pub locked: bool,
+}
+
+pub struct Transaction {
+    amount: f64,
+    trx_type: TransactionType,
+}
+
+pub enum TransactionType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+#[derive(Default)]
+pub struct ProjectionStore {
+    clients: HashMap<u16, ClientAccount>,
+    client_transactions: HashMap<(u16, u32), Transaction>,
+}
+
+impl ProjectionStore {
+    pub fn output(&self) -> Vec<String> {
+        self.clients
+            .iter()
+            .map(|(client_id, client)| {
+                format!(
+                    "{}, {}, {}, {}, {}",
+                    client_id, client.available, client.held, client.total, client.locked
+                )
+            })
+            .collect()
+    }
+
+    pub fn update_for_client(&mut self, client_id: u16, event: &Event) {
+        if self.clients.get(&client_id).is_none() {
+            self.clients.insert(client_id, ClientAccount::default());
+        }
+        match event {
+            Event::AmountDeposited { trx_id, amount } => {
+                let client = self.clients.get_mut(&client_id).unwrap();
+                client.total += amount;
+                client.available = client.total - client.held;
+            }
+            Event::AmountWithdrawn { trx_id, amount } => {
+                let client = self.clients.get_mut(&client_id).unwrap();
+                client.total -= amount;
+                client.available = client.total - client.held;
+            }
+            Event::DisputeRaised { trx_id } => {
+                let amount = self
+                    .client_transactions
+                    .get(&(client_id, *trx_id))
+                    .unwrap()
+                    .amount;
+                let client = self.clients.get_mut(&client_id).unwrap();
+                client.held += amount;
+                client.available = client.total - client.held;
+            }
+            Event::DisputeResolved { trx_id } => {
+                let amount = self
+                    .client_transactions
+                    .get(&(client_id, *trx_id))
+                    .unwrap()
+                    .amount;
+                let client = self.clients.get_mut(&client_id).unwrap();
+                client.held -= amount;
+                client.available = client.total - client.held;
+            }
+            Event::ChargebackIssued { trx_id } => {
+                let amount = self
+                    .client_transactions
+                    .get(&(client_id, *trx_id))
+                    .unwrap()
+                    .amount;
+                let client = self.clients.get_mut(&client_id).unwrap();
+                client.held = 0.0;
+                client.total -= amount;
+                client.available = client.total - client.held;
+                client.locked = true;
+            }
+        }
+    }
+
+    pub fn insert_transaction(
+        &mut self,
+        client_id: u16,
+        trx_id: u32,
+        trx_type: TransactionType,
+    ) -> bool {
+        self.client_transactions
+            .insert(
+                (client_id, trx_id),
+                Transaction {
+                    amount: 0.0,
+                    trx_type,
+                },
+            )
+            .is_some()
+    }
+
+    pub fn get_client_account(&mut self, client_id: u16) -> &ClientAccount {
+        self.clients
+            .entry(client_id)
+            .or_insert_with(ClientAccount::default)
+    }
+
+    pub fn get_client_transaction(&self, client_id: u16, trx_id: u32) -> Option<&Transaction> {
+        self.client_transactions.get(&(client_id, trx_id))
+    }
+}
